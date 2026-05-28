81b48126ff3ff8f911d489af164c4010d037cbdc
Kosar Ghaffarian <kosarghaffarian@gmail.com>
2026-05-28 17:22:39 +0330

    fix: fix navigation menu width

--- app/components/NavMenu.vue
+++ app/components/NavMenu.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
-import { Home, ShoppingBasket, User, Package } from "@lucide/vue";
+import { Home, ShoppingBasket, User, Package, Heart } from "@lucide/vue";
 
 const route = useRoute();
 </script>
 
 <template>
   <NavigationMenu
-    class="fixed bottom-0 left-0 right-0 z-50 border-t bg-white shadow-xl w-full"
+    class="fixed bottom-0 left-0 right-0 z-50 border-t bg-white shadow-xl w-screen max-w-none"
   >
-    <NavigationMenuList class="w-95 flex justify-between px-6 py-2">
+    <NavigationMenuList
+      class="flex justify-between px-6 py-2 w-screen max-w-none"
+    >
       <!-- home -->
       <NavigationMenuItem>
         <NuxtLink to="/" :class="route.path === '/cart' && 'text-blue-600'">
           <Home :class="route.path === '/' ? 'text-black' : 'text-blue-700'" />
+        </NuxtLink>
+      </NavigationMenuItem>
+
+      <!-- wish list -->
+      <NavigationMenuItem>
+        <NuxtLink to="/" :class="route.path === '/wishlist' && 'text-blue-600'">
+          <Heart
+            :class="route.path === '/wishlist' ? 'text-black' : 'text-blue-700'"
+          />
         </NuxtLink>
       </NavigationMenuItem>
 
@@ -36,7 +47,10 @@ const route = useRoute();
 
       <!-- products page -->
       <NavigationMenuItem>
-        <NuxtLink to="/products" :class="route.path === '/products' && 'text-blue-600'">
+        <NuxtLink
+          to="/products"
+          :class="route.path === '/products' && 'text-blue-600'"
+        >
           <Package
             :class="route.path === '/products' ? 'text-black' : 'text-blue-700'"
           />
