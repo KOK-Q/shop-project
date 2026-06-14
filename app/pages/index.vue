5f76a818578e48d44b0349527fb56ba405af29a9
Kosar Ghaffarian <kosarghaffarian@gmail.com>
2026-06-14 09:54:00 +0330

    fix: fix index page layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,38 +1,36 @@
 <script setup lang="ts">
 import { ArrowRight } from "@lucide/vue";
 
-const authStore = useAuthStore();
-
 definePageMeta({
   layout: false,
 });
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col justify-evenly items-center px-6">
-    <div class="flex flex-col justify-center items-center">
+  <div class="min-h-screen flex flex-col justify-evenly items-center gap-8 px-6">
+    <div class="flex flex-col justify-center items-center mt-40">
       <!-- Icon -->
       <div
-        class="w-32 h-32 rounded-full shadow-xl flex items-center justify-center mb-10"
+        class="w-33.5 h-33.5 rounded-full shadow-lg border border-gray-100 flex items-center justify-center mb-6"
       >
-        <img src="/images/bag.png" class="w-20 h-20" />
+        <img src="/images/bag.png" class="w-20 h-23" />
       </div>
 
       <!-- Title -->
-      <h1 class="font-bold text-5xl overflow-hidden mb-4">Shoppe</h1>
+      <h1 class="font-bold text-[52px] overflow-hidden mb-4">Shoppe</h1>
 
       <!-- Subtitle -->
-      <p class="text-center">
+      <p class="text-center text-lg">
         Beautiful eCommerce UI Kit<br />
         for your online store
       </p>
     </div>
 
     <!-- CTA -->
-    <div class="w-full space-y-4">
+    <div class="w-full grid gap-5">
       <NuxtLink to="/signup" class="block">
         <Button
-          class="w-full text-lg font-light bg-blue-600 text-white h-13 hover:bg-blue-400 rounded-xl shadow-md active:scale-95 transition"
+          class="w-full text-xl font-light bg-blue-600 text-white h-15 hover:bg-blue-400 rounded-xl shadow-md active:scale-95 transition"
         >
           Let's get started
         </Button>
@@ -44,18 +42,11 @@ definePageMeta({
 
         <NuxtLink to="/login" class="flex items-center justify-center">
           <div
-            class="w-6 h-6 flex items-center justify-center bg-blue-600 rounded-full hover:bg-gray-200 transition"
+            class="w-7.5 h-7.5 flex items-center justify-center bg-blue-600 rounded-full hover:bg-gray-200 transition"
           >
             <ArrowRight class="text-white" />
           </div>
         </NuxtLink>
-      </div>
-
-      <div
-        class="flex items-center justify-center gap-2 text-sm"
-        @click="authStore.logout()"
-      >
-        Logout
       </div>
     </div>
   </div>
